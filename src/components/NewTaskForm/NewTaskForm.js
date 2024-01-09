1bfedaac01f6b5d5ab77e5059a60377f20bc42c0
Nightingale99 <vladislav.yablonski@gmail.com>
2024-01-09 20:57:08 +0700

    last try

--- src/components/NewTaskForm/NewTaskForm.js
+++ src/components/NewTaskForm/NewTaskForm.js
@@ -25,7 +25,7 @@ export default class NewTaskForm extends React.Component {
   };
 
   render() {
-    console.log('жопа');
+    console.log('жоfdпа');
     return (
       <header className="header">
         <h1>todos</h1>
